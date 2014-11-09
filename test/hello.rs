4ef4f14bca7e9d91d6f317ba13ff08cf51bb7346
Steffen Imhof <github@steffen-imhof.de>
2014-11-09 17:08:26 +0100

    A few more tests wrt .as_slice()

--- test/hello.rs
+++ test/hello.rs
@@ -1,5 +1,26 @@
 // My first rust program
 
+fn source() -> Vec<u8> {
+	let mut result = Vec::new();
+	result.push(8);
+	result.push(9);
+	result.push(10);
+	
+	result
+}
+
+fn do_work(input: &[u8]) -> Vec<u8> {
+	let mut result = Vec::new();
+	for b in input.iter().rev() {
+		result.push(*b);
+	}
+	result
+}
+
 fn main() {
-	println!("Hello, world!");
+	
+	let data = source();
+	let reversed = do_work(&*data);
+	
+	println!("Hello, world! {} {}", data, reversed);
 }
