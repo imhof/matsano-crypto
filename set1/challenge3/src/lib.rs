5eb4ff53ad7a7d46007cfbe1bf081959f57facc7
Steffen Imhof <github@steffen-imhof.de>
2014-11-12 20:54:13 +0100

    Make challenge3 work somewhat

--- set1/challenge3/src/lib.rs
+++ set1/challenge3/src/lib.rs
@@ -1,11 +1,17 @@
-use std::num;
+
+fn key_xor(data: &[u8], key: u8) -> Vec<u8> {
+    data.iter().map(|&x| x ^ key ).collect()
+}
 
 fn square_dist(values1: &[f64], values2: &[f64]) -> f64 {    
-    values1.iter().zip(values2.iter()).fold(0.0, |s, (&a, &b)| s + (a - b) * (a - b)).sqrt()
+    values1.iter()
+           .zip(values2.iter())
+           .fold(0.0, |s, (&a, &b)| s + (b/a - 1.0) * (b/a - 1.0))
+           .sqrt()
 }
 
 fn count_letters(data: &[u8]) -> Vec<uint> {
-    let mut act_count = Vec::from_fn(26, |x| 0 );
+    let mut act_count = Vec::from_fn(26, |x| 0 * x );
 
     for c in data.iter() {
         match *c as uint {
@@ -18,8 +24,6 @@ fn count_letters(data: &[u8]) -> Vec<uint> {
     act_count
 }
 
-
-
 fn score(data: &[u8]) -> f64 {
     // normal frequencies of english letters a-z in %
     let nom_frequencies : &[f64] = [ 8.167, 1.492, 2.782, 4.253, 12.702, 2.228, 2.015, 6.094, 6.966, 0.153, 0.772, 4.025,
@@ -28,9 +32,36 @@ fn score(data: &[u8]) -> f64 {
     let act_count = count_letters(data);
     let total = act_count.iter().fold(0, |s, &a| s + a);
     
-    let act_frequencies : Vec<f64> = act_count.iter().map(|&v| (v as f64) / (total as f64)).collect();
+    if total == 0 {
+        // no characters found at all
+        return 100.0f64
+    }
+    
+    let act_frequencies : Vec<f64> = act_count.iter().map(|&v| (v as f64) * 100f64 / (total as f64)).collect();
         
-    0.0
+    (data.len() - total) as f64 + square_dist(nom_frequencies, act_frequencies.as_slice())
+}
+
+pub fn decode_xor(data: &[u8]) -> Vec<u8> {
+    let mut best_score = 100.0f64;
+    let mut best_result = vec![];
+    
+    for key in range(0u8, 255u8) {
+        let plain = key_xor(data, key);
+        let new_score = score(plain.as_slice());
+           
+        if new_score < best_score {
+            best_result = plain;
+            best_score = new_score;
+        }
+    }
+
+    best_result
+}
+
+#[test]
+fn test_key_xor() {
+    assert_eq!(key_xor([255, 128, 0], 64), vec![191, 192, 64]);
 }
 
 #[test]
@@ -49,6 +80,8 @@ fn test_count_letters() {
 
 #[test]
 fn test_score_basics() {
-    assert_eq!(score([]), 0.0);
-//    assert!(score("this is the end, my friend".as_bytes()) > score("7ufhgvbnftz56ufgvghtzuh54".as_bytes()) );
+    assert_eq!(score([]), score("232093582§$%$!-_.,".as_bytes()) );
+    assert!(score([]) > score("A".as_bytes()) );
+    assert!(score("this is the end, my friend".as_bytes()) < score("7ufhgvbnftz56ufgvghtzuh54".as_bytes()) );
+    assert_eq!(score("this is the end, my friend".as_bytes()), score("endmythisfriend!!!theis.".as_bytes()) );
 }
--- set1/challenge3/src/main.rs
+++ set1/challenge3/src/main.rs
@@ -1,5 +1,25 @@
+// Crypto challenge Set1 / Challenge 3
+// Decrypt single XOR chiffre
+
+extern crate challenge1;
+extern crate challenge2;
+extern crate challenge3;
 
 #[cfg(not(test))]
 fn main() {
-    println!("Hello, world!")
+    let args = std::os::args();
+    
+    if args.len() != 2 {
+        println!("USAGE: challenge3 HEX_ENCODED_STRING");
+    } else {
+        let input = args[1].as_slice();
+        match challenge1::from_hex(input) {
+            Err(msg) => println!("Invalid hex string: {}", msg),
+            Ok(binary) => {
+                let result = challenge3::decode_xor(binary.as_slice());
+                println!("Text {}", std::str::from_utf8(result.as_slice()));
+                println!("Raw {}", result);
+            }
+        }
+    }
 }
