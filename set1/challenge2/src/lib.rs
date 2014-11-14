628c050f6f0b2f272ae4c83780f121d2064d5a44
Steffen Imhof <github@steffen-imhof.de>
2014-11-14 22:47:43 +0100

    Update challenge2 to use 'codec' module, too

--- set1/challenge2/src/lib.rs
+++ set1/challenge2/src/lib.rs
@@ -1,24 +1,4 @@
 
-pub fn to_hex(binary: &[u8]) -> String {
-    /*
-    let mut result = String::new();
-
-    for b in binary.iter() {
-        let hex1 = Char::from_digit((*b as uint & 0xf0) >> 4,16);
-        let hex2 = Char::from_digit(*b as uint & 0x0f,16);
-
-        result.push(hex1.unwrap());
-        result.push(hex2.unwrap());
-    }
-
-    result
-    */
-
-    fn to_hex_char(byte: u8) -> char { Char::from_digit(byte as uint, 16).unwrap() }
-
-    binary.iter().fold(String::new(), |mut s, &b| { s.push(to_hex_char(b >> 4)); s.push(to_hex_char(b & 0x0f)); s } )
-}
-
 pub fn fixed_xor(input1: &[u8], input2: &[u8]) -> Option<Vec<u8>> {
 
     let mut result = Vec::new();
@@ -35,17 +15,6 @@ pub fn fixed_xor(input1: &[u8], input2: &[u8]) -> Option<Vec<u8>> {
         None => return Some(result),
         _ => None
     }
-}
-
-#[test]
-fn to_hex_basics() {
-    assert_eq!(to_hex([]), "".to_string());
-    assert_eq!(to_hex([0]), "00".to_string());
-    assert_eq!(to_hex([1]), "01".to_string());
-    assert_eq!(to_hex([16]), "10".to_string());
-    assert_eq!(to_hex([255]), "ff".to_string());
-    assert_eq!(to_hex([255,1]), "ff01".to_string());
-
 }
 
 #[test]
--- set1/challenge2/src/main.rs
+++ set1/challenge2/src/main.rs
@@ -1,21 +1,21 @@
 
-extern crate challenge1;
+extern crate codec;
 extern crate challenge2;
 
-use challenge2::to_hex;
-use challenge2::fixed_xor;
 
 #[cfg(not(test))]
 fn main() {
-	let test = challenge1::from_hex("AABB99");
+	let test = codec::from_hex("AABB99");
 	println!("Hello, world! {}", test);
 }
 
 #[test]
 fn matsano_test() {
-	let input1 = challenge1::from_hex("1c0111001f010100061a024b53535009181c").ok().unwrap();
-	let input2 = challenge1::from_hex("686974207468652062756c6c277320657965").ok().unwrap();
+	use challenge2::fixed_xor;
+
+	let input1 = codec::from_hex("1c0111001f010100061a024b53535009181c").ok().unwrap();
+	let input2 = codec::from_hex("686974207468652062756c6c277320657965").ok().unwrap();
 	let result = "746865206b696420646f6e277420706c6179".to_string();
 	
-	assert_eq!( to_hex(fixed_xor(input1.as_slice(), input2.as_slice()).unwrap().as_slice()), result);
+	assert_eq!( codec::to_hex(fixed_xor(input1.as_slice(), input2.as_slice()).unwrap().as_slice()), result);
 }
