5178fbaef491e7e5a2cb50a4f4d8afabe5f9e29c
Steffen Imhof <github@steffen-imhof.de>
2014-11-14 22:38:25 +0100

    New module 'codec'
    Moved code from challenge1 to new module and change challenge1 to use it.

--- modules/codec/src/lib.rs
+++ modules/codec/src/lib.rs
@@ -22,6 +22,21 @@ pub fn from_hex(hex_in: &str) -> Result<Vec<u8>, &'static str>
     Ok(bin_out)
 }
 
+
+pub fn to_hex(binary: &[u8]) -> String {
+
+    fn to_hex_char(byte: u8) -> char { Char::from_digit(byte as uint, 16).unwrap() }
+
+    binary.iter().fold(String::new(),
+        |mut s, &b| {
+            s.push(to_hex_char(b >> 4));
+            s.push(to_hex_char(b & 0x0f));
+            s
+        }
+    )
+}
+
+
 fn encode_single_base64(byte_in: u8) -> Option<char> {
     match byte_in {
         0...25 => Some((65u8 + byte_in) as char),
@@ -32,6 +47,21 @@ fn encode_single_base64(byte_in: u8) -> Option<char> {
         _ => None
     }
 }
+
+#[test]
+fn test_encode_single_base64_basics() {
+    assert_eq!(encode_single_base64(0), Some('A'));
+    assert_eq!(encode_single_base64(27), Some('b'));
+    assert_eq!(encode_single_base64(61), Some('9'));
+    assert_eq!(encode_single_base64(63), Some('/'));
+}
+
+#[test]
+fn test_encode_single_base64_fail() {
+    assert_eq!(encode_single_base64(64), None);
+    assert_eq!(encode_single_base64(255), None);
+}
+
 
 pub fn to_base64(bin_in: &[u8]) -> String {
     let mut result = String::new();
@@ -57,22 +87,11 @@ pub fn to_base64(bin_in: &[u8]) -> String {
     result
 }
 
-#[test]
-fn encode_single_base64_basics() {
-    assert_eq!(encode_single_base64(0), Some('A'));
-    assert_eq!(encode_single_base64(27), Some('b'));
-    assert_eq!(encode_single_base64(61), Some('9'));
-    assert_eq!(encode_single_base64(63), Some('/'));
-}
+
+
 
 #[test]
-fn encode_single_base64_fail() {
-    assert_eq!(encode_single_base64(64), None);
-    assert_eq!(encode_single_base64(255), None);
-}
-
-#[test]
-fn from_hex_basics() {
+fn test_from_hex_basics() {
     assert_eq!(from_hex(""), Ok(vec![]));
     assert_eq!(from_hex("01"), Ok(vec![1]));
     assert_eq!(from_hex("ff"), Ok(vec![255]));
@@ -81,15 +100,15 @@ fn from_hex_basics() {
 }
 
 #[test]
-fn from_hex_fail() {
+fn test_from_hex_fail() {
     assert_eq!(from_hex("A"), Err("Odd string length in hex encoded value."));
     assert_eq!(from_hex("ABCDEFGH"), Err("Invalid hex characters encountered."));
     assert_eq!(from_hex("+#"), Err("Invalid hex characters encountered."));
 }
 
 #[test]
-fn to_base64_rfc4648() {
-   assert_eq!(to_base64([]), String::from_str(""));// ""
+fn test_to_base64_rfc4648() {
+   assert_eq!(to_base64([]), String::from_str(""));
    assert_eq!(to_base64("f".as_bytes()), String::from_str("Zg=="));
    assert_eq!(to_base64("fo".as_bytes()), String::from_str("Zm8="));
    assert_eq!(to_base64("foo".as_bytes()), String::from_str("Zm9v"));
@@ -99,8 +118,11 @@ fn to_base64_rfc4648() {
 }
 
 #[test]
-fn challenge1() {
-    let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-    let output = to_base64( from_hex(input).ok().unwrap().as_slice() );
-    assert_eq!(output, String::from_str("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"));
+fn test_to_hex_basics() {
+    assert_eq!(to_hex([]), "".to_string());
+    assert_eq!(to_hex([0]), "00".to_string());
+    assert_eq!(to_hex([1]), "01".to_string());
+    assert_eq!(to_hex([16]), "10".to_string());
+    assert_eq!(to_hex([255]), "ff".to_string());
+    assert_eq!(to_hex([255,1]), "ff01".to_string());
 }
--- set1/challenge1/src/main.rs
+++ set1/challenge1/src/main.rs
@@ -1,7 +1,7 @@
 // Crypto challenge Set1 / Challenge 1
 // Convert hex to base64
 
-extern crate challenge1;
+extern crate codec;
 
 #[cfg(not(test))]
 fn main() {
@@ -11,9 +11,16 @@ fn main() {
         println!("USAGE: challenge1 HEX_ENCODED_STRING");
     } else {
         let input = args[1].as_slice();
-        match challenge1::from_hex(input) {
+        match codec::from_hex(input) {
             Err(msg) => println!("Invalid hex string: {}", msg),
-            Ok(binary) => println!("{}", challenge1::to_base64(binary.as_slice()))
+            Ok(binary) => println!("{}", codec::to_base64(binary.as_slice()))
         }
     }
+}
+
+#[test]
+fn challenge1() {
+    let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    let output = codec::to_base64( codec::from_hex(input).ok().unwrap().as_slice() );
+    assert_eq!(output, String::from_str("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"));
 }
